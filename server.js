07fec5ef5e5c42703ce3b6454aded659d6575502
Nitish Jha <nitishkumar0914@gmail.com>
2024-06-08 13:30:04 +0530

    .env not uploaded

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = require('./app.js');
 
 // const url = 'mongodb+srv://$_USERNAME_$:$_PASSWORD_$@cluster0.dpithd6.mongodb.net/$_DB_NAME_$?retryWrites=true&w=majority&appName=Cluster0';
-const url = process.env.DB_URL;
+const url = "mongodb+srv://nitishkumar0914:Kt20hhjoUHJAa3kg@cluster0.moge4ap.mongodb.net/mern_kiet_image";
 
 // let dbLink = url.replace("$_USERNAME_$", process.env.DB_USER);
 // dbLink = dbLink.replace("$_PASSWORD_$", process.env.DB_PASSWORD);
